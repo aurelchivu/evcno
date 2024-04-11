574ea65a8ec02d28dc27cb0474d7857e2e08275f
aurel <aurelchivuutcb@gmail.com>
2024-04-11 20:15:34 +0300

    First commit

---
+++ evcno-write/src/main/java/com/optimiser/evcno/write/EvcnoWriteApp.java
@@ -0,0 +1,11 @@
+package com.optimiser.evcno.write;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvcnoWriteApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EvcnoWriteApp.class, args);
+    }
+}
