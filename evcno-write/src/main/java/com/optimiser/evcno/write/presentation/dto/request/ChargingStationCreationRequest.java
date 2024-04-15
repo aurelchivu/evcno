b0c390bd65c8486122001d034c714c0488fa49a2
aurel <aurelchivuutcb@gmail.com>
2024-04-15 21:49:13 +0300

    Add jackson-module-parameter-names to resolve serialization issues

--- evcno-write/src/main/java/com/optimiser/evcno/write/presentation/dto/request/ChargingStationCreationRequest.java
+++ evcno-write/src/main/java/com/optimiser/evcno/write/presentation/dto/request/ChargingStationCreationRequest.java
@@ -1,7 +1,5 @@
 package com.optimiser.evcno.write.presentation.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
@@ -30,21 +28,4 @@ public class ChargingStationCreationRequest {
 
     @Schema(description = "The status of the station: fast operational, maintenance")
     String chargingStationStatus;
-
-    @JsonCreator
-    public ChargingStationCreationRequest(
-            @JsonProperty("name") String name,
-            @JsonProperty("address") String address,
-            @JsonProperty("capacity") int capacity,
-            @JsonProperty("availableChargingPoints") int availableChargingPoints,
-            @JsonProperty("chargingStationType") String chargingStationType,
-            @JsonProperty("chargingStationStatus") String chargingStationStatus
-    ) {
-        this.name = name;
-        this.address = address;
-        this.capacity = capacity;
-        this.availableChargingPoints = availableChargingPoints;
-        this.chargingStationType = chargingStationType;
-        this.chargingStationStatus = chargingStationStatus;
-    }
 }
